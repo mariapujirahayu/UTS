295a365c71ff9d494fdf60ffb77e5c988188d263
mariapujirahayu <mariapujir@gmail.com>
2019-10-18 16:27:08 +0700

    Menambahkan combo box

--- app/src/main/java/com/example/uts/SiswaActivity.java
+++ app/src/main/java/com/example/uts/SiswaActivity.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class SiswaActivity extends AppCompatActivity {
     private Button Submit;
@@ -16,17 +20,23 @@ public class SiswaActivity extends AppCompatActivity {
     private EditText Alamat;
     private EditText Hp;
     private EditText Ket;
-    String txtNis, txtNama, txtAlamat, txtHp, txtKet;
+    private Spinner agm;
+    String txtNis, txtNama, txtAlamat, txtHp, txtKet, txtAgama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siswa);
+
         Nis = findViewById(R.id.edtNis);
         Nama = findViewById(R.id.edtNama);
         Alamat = findViewById(R.id.edtAlamat);
         Hp = findViewById(R.id.edtHp);
         Ket = findViewById(R.id.edtKet);
+        agm = findViewById(R.id.pilAgama);
+
+        final Spinner List = findViewById(R.id.pilAgama);
+
     }
 
     public void tampilData(View view) {
@@ -35,10 +45,11 @@ public class SiswaActivity extends AppCompatActivity {
         txtAlamat = Alamat.getText().toString();
         txtHp = Hp.getText().toString();
         txtKet = Ket.getText().toString();
+        txtAgama = agm.getSelectedItem().toString();
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder
-                .setMessage("Nis : " + txtNis + "\nNama : " + txtNama + "\nAlamat : " + txtAlamat + "\nHandphone : " + txtHp + "\nKeterangan : " + txtKet)
+                .setMessage("Nis : " + txtNis + "\nNama : " + txtNama + "\nAlamat : " + txtAlamat + "\nHandphone : " + txtHp + "\nKeterangan : " + txtKet + "\nAgama : " + txtAgama)
                 .setIcon(R.mipmap.ic_launcher)
                 .setCancelable(false)
                 .setPositiveButton("Close", new DialogInterface.OnClickListener() {
